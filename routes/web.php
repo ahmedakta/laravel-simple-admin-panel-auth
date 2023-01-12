d081a664175ee494cb2e845728f4c475e3a1ca16
Ahmet Akta <ahmet@ferisoft.com>
2023-01-13 00:55:02 +0300

    boilerplate

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('home');
 });
-Route::group(['prefix'=>'admin','middleware'=>['isAdmin','auth']],function(){
+Route::group(['prefix'=>'admin','middleware'=>['isAdmin','auth','verified']],function(){
     Route::get('dashboard', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('users-detail', [App\Http\Controllers\AdminController::class, 'users_detail'])->name('users.detail');
     Route::get('customers', [App\Http\Controllers\AdminController::class, 'customers'])->name('customers');
@@ -34,5 +34,5 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-Route::get('/verify', [App\Http\Controllers\HomeController::class, 'verify'])->name('verify');
+    
+// Route::get('/verify', [App\Http\Controllers\HomeController::class, 'verify'])->name('verify');
